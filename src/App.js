dc59939ac3e6762f75ccec86c88e6fe441b16ab5
mriansabado <djian808@gmail.com>
2022-06-22 23:04:07 -0700

    added more bootstrap elements and writing in index.html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Aloha Friends</h1>
-      
-    </div>
-  );
+  return(
+    <>
+      <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Ian Sabado</Navbar.Brand>
+    <Nav className="me-right">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+    </>
+  )
 }
 
 
